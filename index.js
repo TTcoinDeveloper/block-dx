4e50e7295d158f73c4a84a9f1f331e85194d4432
rBurgett <ryan@burgettweb.net>
2019-06-05 13:36:16 -0400

    Added wallet and balance refresh buttons plus balances tooltip

--- index.js
+++ index.js
@@ -1071,6 +1071,17 @@ const openAppWindow = () => {
   ipcMain.on('getBalances', () => sendBalances(true));
   setInterval(sendBalances, stdInterval);
 
+  ipcMain.on('refreshBalances', async function() {
+    try {
+      await loadXBridgeConf();
+      await sendNetworkTokens();
+      await sendLocalTokens();
+      await sendBalances(true);
+    } catch(err) {
+      console.error(err);
+    }
+  });
+
   sendPricingMultipliers = async function() {
     try {
       if(!enablePricing) {
@@ -1200,9 +1211,13 @@ ipcMain.on('saveGeneralSettings', (e, s) => {
   sendMarketPricingEnabled();
 });
 
+const loadXBridgeConf = async function() {
+  await sn.dxLoadXBridgeConf();
+};
+
 ipcMain.on('loadXBridgeConf', async function() {
   try {
-    await sn.dxLoadXBridgeConf();
+    await loadXBridgeConf();
   } catch(err) {
     console.error(err);
   }
